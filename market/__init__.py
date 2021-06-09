d1cbf1f8088473e3b91ba9804d23328a53d211f8
Adnan Farooque <adnan.dipta89@gmail.com>
2021-06-10 02:29:08 +0600

    initial commit

---
+++ market/__init__.py
@@ -0,0 +1,15 @@
+from os import environ
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or "sqlite:///market.db"
+app.config['SECRET_KEY'] = "baccafcd98d5c3d53657c402"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
